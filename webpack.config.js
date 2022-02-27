77e88e758948c7464abdd3c89f808c62def7292d
Bastien Ricard <bastienricard@macbook-pro-de-bastien.home>
2022-02-27 13:25:48 +0100

    Fix synthax error in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.export = {
+module.exports = {
   entry: {
     main: path.join(__dirname, "src/index.js"),
   },
